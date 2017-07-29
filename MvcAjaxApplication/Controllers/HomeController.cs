4ee8f25abc1a0012eca8aeff208dab2a3e26b92a
Chandan Kumar Baba <cbaba20@gmail.com>
2017-07-29 15:25:40 +0530

    added master code to learn Ajax calling

---
+++ MvcAjaxApplication/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAjaxApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        DemoApplicationDBEntities db = new DemoApplicationDBEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            //Product[] _product = db.Products.ToArray();
+
+            return View(); //_product
+        }
+
+        public PartialViewResult GetProductDescription()
+        {
+            var productList = db.Products.ToList();
+
+            return PartialView(productList);
+        }
+
+        [HttpGet]
+        public PartialViewResult ChangeProductPrice(int productId,decimal newPrice)
+        {
+            var _productList = db.Products.Where(p => p.ProductId == productId).ToList();
+            Product product = _productList.FirstOrDefault();
+            product.Price = newPrice;
+            db.SaveChanges();
+            return PartialView("GetProductDescription", _productList);
+        }
+    }
+}
---
+++ MvcAjaxApplication/Models/Products.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Linq.Mapping;
+namespace MvcAjaxApplication.Models
+{
+    [Table(Name = "Products")]
+    public class Products
+    {
+        [Column(IsPrimaryKey =true)]
+        public int productId { get; set; }
+        [Column]
+        public string productName { get; set; }
+        [Column]
+        public string productDescription { get; set; }
+        [Column]
+        public decimal Price { get; set; }
+        [Column]
+        public int UnitsInStock { get; set; }
+
+    }
+}
